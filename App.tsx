49368bd76ae3cf39fa6ea53f27089df7684350e1
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-22 19:03:13 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.eventName} >
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 16,
   },
-});
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  },
+})

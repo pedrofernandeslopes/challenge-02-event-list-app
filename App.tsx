16a81db3c3d3d15bd805ec53accc61e6a7d97479
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-22 19:09:02 -0300

    chore: created Home screen

--- App.tsx
+++ App.tsx
@@ -1,32 +1,7 @@
-import { View, Text, StyleSheet } from 'react-native';
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName} >
-        Nome do evento
-      </Text>
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-    </View>
+    <Home />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 16,
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48,
-  },
-  eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16,
-  },
-})

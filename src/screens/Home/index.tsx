e628eed8ed8d4f1b6190412c746e06f7ba0bb194
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-22 19:44:07 -0300

    chore: added input to home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
-export default function Home() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName} >
-        Nome do evento
-      </Text>
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-    </View>
-  )
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName} >
+                Nome do evento
+            </Text>
+
+            <Text style={styles.eventDate}>
+                Sexta, 4 de Novembro de 2022.
+            </Text>
+
+            <TextInput
+                style={styles.input}
+                placeholder='Nome do participante'
+                placeholderTextColor='#6B6B6B'
+            />
+        </View>
+    )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
       padding: 16,
     },
     eventName: {
-      color: '#FDFCFE',
+      color: '#FFF',
       fontSize: 24,
       fontWeight: 'bold',
       marginTop: 48,
@@ -16,5 +16,14 @@ export const styles = StyleSheet.create({
       color: '#6B6B6B',
       fontSize: 16,
     },
+    input : {
+        height: 56,
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16,
+    },
+    
   })
 
